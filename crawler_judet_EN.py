b411e7e70523ba813213c22cae42bcda56d96c17
mircea007 <mirceareb@gmail.com>
2022-07-01 15:24:53 +0300

    simpler ui and more flexible command line options

--- crawler_judet_EN.py
+++ crawler_judet_EN.py
@@ -1,5 +1,5 @@
 from math import inf
-import sys
+import sys, getopt
 import os
 from selenium import webdriver
 import time
@@ -7,6 +7,7 @@ import time
 NUM_ROWS = 20
 
 num_pages, output = None, None
+verbose = False
 
 os.environ['PATH'] = os.environ['PATH'] + ":."
 
@@ -15,8 +16,10 @@ js_script_next = open("js/next.js", "r").read()
 js_script_last = open("js/last.js", "r").read()
 js_script_first = open("js/first.js", "r").read()
 
-browser = webdriver.Firefox()
-browser.install_addon('web-extensions/ublock_origin-1.43.0.xpi', temporary=True)
+def init_browser():
+  global browser
+  browser = webdriver.Firefox()
+  browser.install_addon('web-extensions/ublock_origin-1.43.0.xpi', temporary=True)
 
 def prev_page():
   browser.execute_script("history.back()")
@@ -41,7 +44,8 @@ def next_page():
       tr = browser.execute_script("return document.getElementsByClassName('tr1').length")
 
     if tr == 0:
-      print('failed to load next page, going back')
+      if verbose:
+        print('failed to load next page, going back')
       fails += 1
       prev_page()
 
@@ -57,10 +61,12 @@ def adjust_page(id):
   while page_id() != id:
     while page_id() < id:
       next_page()
-      print('Next page')
+      if verbose:
+        print('Next page')
     while page_id() > id:
       prev_page()
-      print('Prev page')
+      if verbose:
+        print('Prev page')
 
 def extract_data(pid):
   fails = 5
@@ -68,13 +74,15 @@ def extract_data(pid):
     data = browser.execute_script(js_script_extract)
     fails = 0
     while fails < 5 and pid < num_pages and data != None and data.count('\n') < NUM_ROWS:
-      print('Data not fetched correctly, waiting and retrying')
-      print(data)
+      if verbose:
+        print('Data not fetched correctly, waiting and retrying')
+        print(data)
       fails += 1
       time.sleep(1)
       data = browser.execute_script(js_script_extract)
     if fails == 5:
-      print('Failed too much, realoading page...')
+      if verbose:
+        print('Failed too much, realoading page...')
       if pid != 1:
         prev_page()
       next_page()
@@ -110,26 +118,55 @@ def get_num_pages():
 
   return retval
 
-def main(argv):
-  if len(argv) != 3:
-    print("Usage: python %s download_url output_file" % (argv[0]))
-    return
+def usage():
+  print("Usage: python %s [-h|--help] [-v] [--judet <judet>] -o|--output <output_file> <download_url>" % (sys.argv[0]))
+  if 'browser' in globals():
+    global browser
+    browser.close()
+  sys.exit( 1 )
 
-  browser.get(argv[1])
+def main(argv):
+  fout = 'out.csv'
+  url = ''
+  extra = ''
+  global verbose
+  verbose = False
+
+  try:
+    opts, args = getopt.getopt( argv[1:], "hvo:", ["help", "output=", "judet="] )
+  except getopt.GetoptError:
+    usage()
+
+  if len( args ) == 0:
+    usage()
+
+  url = args[0]
+  for opt, arg in opts:
+    if opt in ('-h', '--help'):
+      usage()
+    elif opt == '-v':
+      verbose = True
+    elif opt in ('-o', '--output'):
+      fout = arg
+    elif opt == '--judet':
+      extra = ' (' + arg + ')'
+
+  init_browser()
+  browser.get( url )
   global num_pages, output
 
   num_pages = get_num_pages()
 
-  output = open(argv[2], "a", encoding="utf-8")
+  output = open(fout, "a", encoding="utf-8")
 
   for i in range(1, num_pages + 1):
     output.write(extract_data(i))
-    print("Loaded page %d/%d" % (i, num_pages))
+    print("Loaded page %d/%d%s" % (i, num_pages, extra + 10 * " "), end = ("\n" if verbose else "\r"))
     if i < num_pages:
       next_page()
       adjust_page(i + 1)
 
-  print("Job done!")
+  print("Job done!" if verbose else "\n")
   browser.close()
 
 if __name__ == "__main__":
