2414a67008c4989c4bcbea682bc60c28ac076d64
Tiberiu <musattiberiu@gmail.com>
2024-02-28 20:03:04 +0100

    latlong crawl

--- info_crawler.py
+++ info_crawler.py
@@ -5,10 +5,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 import urllib.parse
-import time
 from judete import judete_dupa_cod
 from tqdm import tqdm
-import random
+import time
 
 
 load_dotenv()
@@ -16,10 +15,9 @@ load_dotenv()
 conn = sqlite3.connect(os.getenv("DB_FILE"))
 cur = conn.cursor()
 
-licee = cur.execute("SELECT id_liceu, nume_liceu FROM LICEE").fetchall()
+institutii = cur.execute("SELECT id, nume, adresa, longlat FROM institutii").fetchall()
 
 MAPS_URL = "https://www.google.com/maps?output=search&hl=ro&q="
-# SEARCH_URL = "https://www.bing.com/search?q="
 
 chrome_options = Options()
 chrome_options.add_extension("web-extensions/uBlock-Origin.crx")
@@ -27,18 +25,14 @@ browser = webdriver.Chrome(options=chrome_options)
 
 consent = False
 
-for id_liceu, nume_liceu in tqdm(licee):
-    address = None
-    website = None
-    facebook = None
-    instagram = None
-    wikipedia = None
+# !3m1!4b1!4m6!3m5!1s0x40b202033db033df:0x1cb72daf776fb289!8m2!3d44.4580628!4d26.080109!16s%2Fm%2F0cmcm8k
 
-    cod_judet = id_liceu.split("_")[-1]
-    judet = judete_dupa_cod[cod_judet]["nume_complet"]
 
+def try_get_longlat(query):
     try:
-        browser.get(MAPS_URL + urllib.parse.quote(nume_liceu + " " + judet))
+        global consent
+
+        browser.get(MAPS_URL + urllib.parse.quote(query))
 
         if not consent:
             browser.execute_script(
@@ -46,55 +40,54 @@ for id_liceu, nume_liceu in tqdm(licee):
             )
             consent = True
 
-        WebDriverWait(browser, 10).until(
+        WebDriverWait(browser, 3).until(
             lambda driver: driver.execute_script(
                 "return document.querySelector(\"[data-tooltip='Copiază adresa']\") != null"
             )
         )
 
-        address = browser.execute_script(
-            "return document.querySelector(\"[data-tooltip='Copiază adresa']\").innerText"
-        )
+        url = browser.current_url
+        while not url.count("@") == 1:
+            time.sleep(0.1)
+            url = browser.current_url
+        data = url.split("/data=")[1].split("?")[0]
+        long, lat = data.split("!")[7:9]
+        long = float(long[2:])
+        lat = float(lat[2:])
 
-        website = browser.execute_script(
-            "let o = document.querySelector(\"[data-tooltip='Deschide site-ul']\"); return o ? o.getAttribute('href') : null"
-        )
+        time.sleep(0.1)
+
+        return long, lat
+
+    except Exception as e:
+        return None, None
+
+
+for id, nume, adresa, ll in tqdm(institutii):
+    if ll is not None:
+        continue
+
+    cod_judet = id.split("_")[-1]
+    judet = judete_dupa_cod[cod_judet]["nume_complet"]
+
+    try:
+        long, lat = try_get_longlat(nume + " " + judet)
+
+        if long is None:
+            print(f" Getting by name failed for {id}, trying by address...")
+            long, lat = try_get_longlat(adresa + " " + judet)
+            if long is None:
+                print("Failed by address too, skipping...")
+
+        if long is not None:
+            longlat = str(long) + "," + str(lat)
+            cur.execute(
+                f"UPDATE institutii SET longlat = ? WHERE id = ?",
+                (longlat, id),
+            )
+            conn.commit()
 
     except Exception as e:
         pass
-
-    # browser.get(SEARCH_URL + urllib.parse.quote(nume_liceu + " " + judet))
-
-    # WebDriverWait(browser, 10).until(
-    #     lambda driver: driver.execute_script(
-    #         "return document.readyState === 'complete'"
-    #     )
-    # )
-
-    # facebook = browser.execute_script(
-    #     "return [...document.querySelectorAll('a')].map(a => a.href).filter(h => h !='').filter(h => new URL(h).host == 'www.facebook.com')[0]"
-    # )
-    # instagram = browser.execute_script(
-    #     "return [...document.querySelectorAll('a')].map(a => a.href).filter(h => h !='').filter(h => new URL(h).host == 'www.instagram.com')[0]"
-    # )
-    # wikipedia = browser.execute_script(
-    #     "return [...document.querySelectorAll('a')].map(a => a.href).filter(h => h !='').filter(h => new URL(h).host == 'ro.wikipedia.org' || new URL(h).host == 'en.wikipedia.org')[0]"
-    # )
-
-    # print(id_liceu, facebook, instagram, wikipedia)
-
-    for field, value in [
-        ("address", address),
-        ("website", website),
-        ("facebook", facebook),
-        ("instagram", instagram),
-        ("wikipedia", wikipedia),
-    ]:
-        if value is not None:
-            cur.execute(
-                f"UPDATE LICEE SET {field} = ? WHERE id_liceu = ?",
-                (value, id_liceu),
-            )
-    conn.commit()
 
 # browser.quit()
