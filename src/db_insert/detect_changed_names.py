5c0c9295de4f2abacccd5485e655dc782f6b56c7
Tiberiu <musattiberiu@gmail.com>
2024-11-09 13:52:37 +0100

    name changes

--- src/db_insert/detect_changed_names.py
+++ src/db_insert/detect_changed_names.py
@@ -2,7 +2,7 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-from utils.gpt_name_formatter import cannonicalize_name
+from utils.gpt_name_formatter import cannonicalize_name, format_nume_advanced
 
 
 load_dotenv()
@@ -18,7 +18,10 @@ if __name__ == "__main__":
     cur = conn.cursor()
 
     an = 2024
-    filter_cond = f"an = {an} AND promotie_anterioara = False"
+    table = "en"
+    filter_cond = (
+        f"an = {an} AND promotie_anterioara = False" if table == "bac" else f"an = {an}"
+    )
 
     # Load existing entries
     cur.execute("SELECT cod_siiir, nume, cod_judet FROM institutii")
@@ -28,13 +31,14 @@ if __name__ == "__main__":
     print(f"Loaded {len(existing)} existing institutii")
 
     cur.execute(
-        f"SELECT unitate_siiir, unitate_nume, unitate_cod_judet FROM bac WHERE {filter_cond} GROUP BY unitate_siiir, unitate_nume, unitate_cod_judet"
+        f"SELECT unitate_siiir, unitate_nume, unitate_cod_judet FROM {table} WHERE {filter_cond} GROUP BY unitate_siiir, unitate_nume, unitate_cod_judet"
     )
     institutii_en = cur.fetchall()
 
     print(f"Loaded {len(institutii_en)} institutii_en")
 
     found = 0
+    differences = 0
     for siiir, nume, cod_judet in institutii_en:
         if siiir not in existing_dict:
             print(
@@ -49,15 +53,38 @@ if __name__ == "__main__":
         else:
             existing_name = existing_dict[siiir][1]
             existing_cod_judet = existing_dict[siiir][2]
-            if cannonicalize_name(
-                nume, existing_cod_judet, id=True
-            ) != cannonicalize_name(existing_name, cod_judet, id=True):
+
+            cname_new = (
+                cannonicalize_name(nume, cod_judet, id=True)
+                .replace("_", "")
+                .replace("A", "I")
+            )
+            cname_old = (
+                cannonicalize_name(existing_name, existing_cod_judet, id=True)
+                .replace("_", "")
+                .replace("A", "I")
+            )
+            if cname_new != cname_old:
                 print(
                     f"Found different name '{nume}' for existing school '{existing_name}', SIIIR code {siiir}"
                 )
+                differences += 1
+
+                new_name = format_nume_advanced(nume, liceu=False)
+                print(f"Setting new name: {new_name}")
+                print("")
+                cur.execute(
+                    "UPDATE institutii SET nume = %s WHERE cod_siiir = %s",
+                    (new_name, siiir),
+                )
+                conn.commit()
+
+                # if differences > 10:
+                #     exit()
             found += 1
 
     print(f"Found {found} existing schools")
+    print(f"Found {differences} differences")
 
     # conn.commit()
     conn.close()
--- src/db_insert/utils/gpt_name_formatter.py
+++ src/db_insert/utils/gpt_name_formatter.py
@@ -8,11 +8,11 @@ from tqdm import tqdm
 
 # from unification import cannonicalize_name
 
-# load_dotenv()
+load_dotenv()
 
-# from openai import OpenAI
+from openai import OpenAI
 
-# client = OpenAI()
+client = OpenAI()
 
 
 def cannonicalize_name(liceu, cod_judet, id=False) -> str:
@@ -75,13 +75,13 @@ def cannonicalize_name(liceu, cod_judet, id=False) -> str:
     return liceu
 
 
-def gpt_liceu(name, gpt4=False):
+def gpt_liceu(name):
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo" if not gpt4 else "gpt-4-1106-preview",
+        model="gpt-4o",
         messages=[
             {
                 "role": "user",
-                "content": "Put the following names of Romanian high schools in the correct format, respecting the diacritics and the use of capital letters. Place a comma between the high school name and the county name, if the county name appears. If abbreviated words appear, do not expand them. Keep the exact form of the words. Don't remove words. Answer only with the correctly formatted name of the high school.",
+                "content": "Put the following names of Romanian high schools in the correct format, respecting the diacritics and the use of capital letters. If the school name contains the name of a person, surround it with quotes. Place a comma between the high school name and the county name, if the county name appears. If abbreviated words appear, do not expand them. Keep the exact form of the words. Don't remove words. Answer only with the correctly formatted name of the high school.",
             },
             {"role": "user", "content": "COLEGIUL NATIONAL, IASI"},
             {"role": "assistant", "content": "Colegiul Național, Iași"},
@@ -102,6 +102,14 @@ def gpt_liceu(name, gpt4=False):
             },
             {
                 "role": "user",
+                "content": 'LICEUL "TIMOTEI CIPARIU" DUMBRĂVENI',
+            },
+            {
+                "role": "assistant",
+                "content": 'Liceul "Timotei Cipariu", Dumbrăveni',
+            },
+            {
+                "role": "user",
                 "content": 'LICEUL TEORETIC DE INFORMATICA "GHEORGHE SINCAI", IASI',
             },
             {
@@ -116,9 +124,25 @@ def gpt_liceu(name, gpt4=False):
                 "role": "assistant",
                 "content": 'Seminarul Teologic Ortodox "Sf. Ioan Gură de Aur", Târgoviște',
             },
+            {
+                "role": "user",
+                "content": "ȘCOALA GIMNAZIALĂ TEMPFLI JOZSEF URZICENI",
+            },
+            {
+                "role": "assistant",
+                "content": 'Școala Gimnazială "Tempfli József", Urziceni',
+            },
+            {
+                "role": "user",
+                "content": "SCOALA GIMNAZIALA AVRAM IANCU AVRAM IANCU",
+            },
+            {
+                "role": "assistant",
+                "content": 'Școala Gimnazială "Avram Iancu", Avram Iancu',
+            },
             {"role": "user", "content": name},
         ],
-        temperature=0 if not gpt4 else 0.5,
+        temperature=0,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
@@ -128,7 +152,7 @@ def gpt_liceu(name, gpt4=False):
     return response.choices[0].message.content
 
 
-def gpt_scoala(name, gpt4=False):
+def gpt_scoala(name):
     examples = [
         ["LICEUL TIMOTEI CIPARIU DUMBRĂVENI", 'Liceul "Timotei Cipariu", Dumbrăveni'],
         ["ȘCOALA GIMNAZIALĂ NR. 7 MEDIAȘ", "Școala Gimnazială Nr. 7, Mediaș"],
@@ -142,9 +166,13 @@ def gpt_scoala(name, gpt4=False):
             "SCOALA GIMNAZIALA AVRAM IANCU AVRAM IANCU",
             'Școala Gimnazială "Avram Iancu", Avram Iancu',
         ],
+        [
+            "LICEUL TEHNOLOGIC COMUNA RUŞEŢU",
+            "Liceul Tehnologic, comuna Rușețu",
+        ],
     ]
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo" if not gpt4 else "gpt-4-1106-preview",
+        model="gpt-4o",
         messages=[
             {
                 "role": "user",
@@ -172,16 +200,15 @@ def gpt_scoala(name, gpt4=False):
     return response.choices[0].message.content
 
 
-def name_sanity_check(name, id):
-    cod_judet = id.split("_")[-1]
-
-    id_nume = cannonicalize_name(name, cod_judet, id=True)
+def name_sanity_check(name, baseline):
+    id_nume = cannonicalize_name(name, "X", id=True)
+    id_baseline = cannonicalize_name(baseline, "X", id=True)
 
     # Merge 'A' and 'I' to allow for Î->Â fixes
-    id_nume = id_nume.replace("A", "I")
-    id = id.replace("A", "I")
+    id_nume = id_nume.replace("_", "").replace("A", "I")
+    id_baseline = id_baseline.replace("_", "").replace("A", "I")
 
-    return id_nume == id
+    return id_nume == id_baseline
 
 
 def format_name_basic(name):
@@ -233,31 +260,33 @@ def format_name_basic(name):
     return name
 
 
-def format_nume_advanced(nume, id, liceu=True):
-    nume = nume.replace("RIMNICU", "RAMNICU").replace("RÎMNICU", "RÂMNICU")
-    nume = nume.replace("TIRGU", "TARGU").replace("TÎRGU", "TÂRGU")
+def format_nume_advanced(nume, liceu=True):
+    nume_initial = nume
 
-    id = id.replace("RIMNICU", "RAMNICU")
-    id = id.replace("TIRGU", "TARGU")
+    nume = (
+        nume.replace("RIMNICU", "RÂMNICU")
+        .replace("RÎMNICU", "RÂMNICU")
+        .replace("Rîmnicu", "Râmnicu")
+        .replace("Rimnicu", "Râmnicu")
+    )
+    nume = (
+        nume.replace("TIRGU", "TÂRGU")
+        .replace("TÎRGU", "TÂRGU")
+        .replace("Tîrgu", "Târgu")
+        .replace("Tirgu", "Târgu")
+    )
 
     nume = format_name_basic(nume)
     # print("\nNume basic:", nume)
-    nume_bun = gpt_liceu(nume) if liceu else gpt_scoala(nume)
+    nume_bun = gpt_liceu(nume.upper()) if liceu else gpt_scoala(nume.upper())
     # print("Nume GPT:", nume_bun)
 
-    if not name_sanity_check(nume_bun, id):
-        # print(f"\nId not equal: {id} vs {nume_bun}")
+    if not name_sanity_check(nume_bun, nume_initial):
+        print(f"\nId still not equal: {id} vs {nume_bun}")
         print(nume_bun)
 
-        # print("\nTrying GPT-4...")
-        nume_bun = gpt_liceu(nume, gpt4=True) if liceu else gpt_scoala(nume, gpt4=True)
-
-        if not name_sanity_check(nume_bun, id):
-            print(f"\nId still not equal: {id} vs {nume_bun}")
-            print(nume_bun)
-
-            print("\nUsing symbolic formatter...")
-            nume_bun = format_name_basic(nume)
+        print("\nUsing symbolic formatter...")
+        nume_bun = format_name_basic(nume)
 
     if nume_bun.count('"') == 2:
         nume_bun = nume_bun.replace('"', "„", 1)
